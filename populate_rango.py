b1dd7264abc2b544556d7f1c4b3fcb3c9f45bce3
Daniela Chambilla <63526399+paodanchacon@users.noreply.github.com>
2023-03-29 23:32:23 -0400

    join models, templates and views

--- populate_rango.py
+++ populate_rango.py
@@ -9,34 +9,42 @@ from rango.models import Category, Page
 def populate():
     python_pages = [
         {'title': 'Official Python Tutorial',
-         'url':'http://docs.python.org/3/tutorial/'},
+         'url':'http://docs.python.org/3/tutorial/',
+         'views': 114},
         {'title':'How to Think like a Computer Scientist',
-         'url':'http://www.greenteapress.com/thinkpython/'},
+         'url':'http://www.greenteapress.com/thinkpython/',
+         'views': 53},
         {'title':'Learn Python in 10 Minutes',
-         'url':'http://www.korokithakis.net/tutorials/python/'}]
+         'url':'http://www.korokithakis.net/tutorials/python/',
+         'views': 20}]
 
     django_pages = [
         {'title':'Official Django Tutorial',
-         'url':'https://docs.djangoproject.com/en/4.0/intro/tutorial01/'},
+         'url':'https://docs.djangoproject.com/en/4.0/intro/tutorial01/',
+         'views': 32},
         {'title':'Django Rocks',
-         'url':'http://www.djangorocks.com/'},
+         'url':'http://www.djangorocks.com/',
+         'views': 14},
         {'title':'How to Tango with Django',
-         'url':'http://www.tangowithdjango.com/'}]
+         'url':'http://www.tangowithdjango.com/',
+         'views': 64}]
 
     other_pages = [
         {'title':'Bottle',
-         'url':'http://bottlepy.org/docs/dev/'},
+         'url':'http://bottlepy.org/docs/dev/',
+         'views': 54},
         {'title':'Flask',
-         'url':'http://flask.pocoo.org'}]
+         'url':'http://flask.pocoo.org',
+         'views': 64}]
 
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages} }
+    cats = {'Python': {'pages': python_pages, 'views': 3, 'likes': 6},
+            'Django': {'pages': django_pages, 'views': 12, 'likes': 24},
+            'Other Frameworks': {'pages': other_pages, 'views': 12, 'likes': 6} }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat)
+        c = add_cat(cat, views=cat_data['views'], likes=cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
 
     for c in Category.objects.all():
         for p in Page.objects.filter(category=c):
@@ -49,8 +57,10 @@ def add_page(cat, title, url, views=0):
         p.save()
         return p
 
-def add_cat(name):
+def add_cat(name, views=0, likes=0):
         c = Category.objects.get_or_create(name=name)[0]
+        c.views = views
+        c.likes = likes
         c.save()
         return c
 
